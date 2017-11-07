4318b90d9480c05c801df4864c5fc46fbe835ac8
Alicia M. Elliott <aliciae@mit.edu>
2017-11-07 18:18:43 -0500

    iterate on orders, mesh spacings

--- src/problem.py
+++ src/problem.py
@@ -6,17 +6,15 @@ from math import pi
 ############# PROBLEM SETUP #############
 #########################################
 pitch = 1.25
-spacing = 0.05                              #mesh spacing
 fwidth = 0.8                                #fuel width/height
 q_fuel = 10 / 4 * pi                        #fuel source
 q_mod = 0                                   #moderator source
 
-#Sn order
-order = 4
+
 
 #convergence tolerance
-tol = 1e-5
-max_iters = 100
+tol = 1e-4
+max_iters = 50
 
 #########################################
 ########## MATERIAL PROPERTIES ##########
@@ -54,14 +52,46 @@ sigma_mod = (2 * xs_absorption_h + xs_scatter_o + 2 * xs_absorption_h) * num_den
 fuel = geometry.Material('fuel', q_fuel, sigma_fuel)
 moderator = geometry.Material('moderator', q_mod, sigma_mod)
 
-#setup mesh cells
-mesh = geometry.Geometry(pitch, spacing, fwidth, fuel, moderator)
-mesh.setMesh()
-
-#give order, mesh to solver
-solve = solver.SN(order, mesh.cells, spacing, mesh.n_cells, mesh.n_fuel, mesh.n_mod, tol, mesh.fuel_area, mesh.mod_area)
-solve.solveSN(max_iters)
-solve.getAvgScalarFlux()
 
 
+#Sn order
+orders = [2, 4, 8]
+spacings = [0.05, 0.01, 0.005]  #mesh spacing
+results = []
 
+order = 4
+i=0
+sptol = 0.001
+rat_old = 0
+for spacing in spacings:
+#for order in orders:
+    print "\nSolving SN, order %d, spacing %g" %(order, spacing)
+    #setup mesh cells
+    mesh = geometry.Geometry(pitch, spacing, fwidth, fuel, moderator)
+    mesh.setMesh()
+
+    #give order, mesh to solver
+    solve = solver.SN(order, mesh.cells, spacing, mesh.n_cells, mesh.n_fuel, mesh.n_mod, tol, mesh.fuel_area, mesh.mod_area)
+    solve.solveSN(max_iters)
+    solve.getAvgScalarFlux()
+    results.append(solve.results)
+    fluxchg = (solve.results[-1] - rat_old)/solve.results[-1]
+    print "flux ratio change: %g" %(fluxchg)
+    if fluxchg <= sptol:
+        print "Spatial mesh is converged!"
+    rat_old = solve.results[-1]
+    i+=1
+
+"""
+i=0
+print "\n\n---------------------------------\nSOLVE RESULTS\n---------------------------------"
+for result in results:
+    print "SN Order: %d\n" %(orders[i])
+    it, ff, mf, af, rat = result
+    #solve.returnSolveResults(it, ff, mf, af, rat)
+
+    print "Iterations to convergence: %d \nModerator flux: %g\nFuel flux: %g\nAvg flux: %g\nFlux ratio: %g"\
+          %(it, mf, ff, af, rat)
+    print "---------------------------------\n"
+    i+=1
+"""
